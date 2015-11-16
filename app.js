09cf1626d4730ced10b7a3aed1a750bd6db0d330
B Hold <brendon.ho01@gmail.com>
2015-11-15 22:54:04 -0500

    ayy

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var app = express();
 app.use(express.static('public'));
 
 app.get("/", function (req,res){
-  res.sendFile("index.html");
+  res.sendFile("login.html");
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen( process.env.PORT||3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
