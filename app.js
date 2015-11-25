b99492b01fa2f2e706e9268362f96e0228447902
Ivan Chau <ivanmchau@gmail.com>
2015-11-25 15:24:10 -0500

    added some mongo stuff for user auth & database

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var nameOfCreator = "Brendon Ho";
+var assert = require('assert');
 var express = require("express");
 var path = require("path");
 var app = express();
 app.use(express.static('public'));
-
+var url = 'mongodb://localhost:27017/test';
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+	console.log('connection success');
+});
 app.get("/", function (req,res){
   res.sendFile("index.html");
 });
