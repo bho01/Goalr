ad5c5381c41d7890e5a98d54f4ef938735ebbc9d
Ivan Chau <ivanmchau@gmail.com>
2015-11-30 20:20:08 -0500

    noidea what

--- models/user.js
+++ models/user.js
@@ -2,14 +2,21 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	passportLocalMongoose = require('passport-local-mongoose');
 
+var Goal = new Schema({
+	goal : {goalName : String,
+			subGoals : {goalName : String, 
+						complete : Bool}},
+	due : Date
+});
 var User = new Schema({
 	username : String,
 	firstName : String,
 	lastName : String,
 	password: String,
 	email : String,
-	goalName : [String]
+	goalName : [Goal]
 });
+
 
 User.plugin(passportLocalMongoose);
 
