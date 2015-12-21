4417f0d8ed80fe248880d8d883f115b526d22fc4
Ivan Chau <ivanmchau@gmail.com>
2015-12-20 22:00:51 -0500

    >

--- Goalr/ViewController.swift
+++ Goalr/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     @IBOutlet weak var remove: UIButton!
     @IBOutlet weak var bottomContainer: UIView!
 
@@ -18,8 +18,23 @@ class ViewController: UIViewController{
     @IBOutlet weak var add: UIButton!
     @IBOutlet weak var slider: UISlider!
     
+    @IBOutlet weak var tableView : UITableView!
+    @IBOutlet weak var goalCount : UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let goalCell = tableView.dequeueReusableCellWithIdentifier("GoalCell")
+        return goalCell!;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
     }
     
    
