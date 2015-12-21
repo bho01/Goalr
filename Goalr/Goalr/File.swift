c13742a54b4868d33c834a3e782f7ba5dbec287a
ClevererMango27 <cleverermango27@gmail.com>
2015-12-20 22:18:17 -0500

    aye

---
+++ Goalr/Goalr/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Goalr
+//
+//  Created by Brendon Duan Ho on 12/20/15.
+//  Copyright © 2015 SwiftlyDoIt. All rights reserved.
+//
+
+import UIKit
+
+class PageItemController: UIViewController{
+    @IBOutlet var contentImageView: UIImageView?
+}
